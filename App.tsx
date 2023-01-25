4b4f5525a3618b04c92f30f362a9650115f0e12b
C1330319 Danylo Alves de Oliveira <C1330319@bb.interno.com.br>
2023-01-25 16:41:27 -0300

    configurações nativas android

--- App.tsx
+++ App.tsx
@@ -10,6 +10,9 @@ import { Loading } from '@components/Loading'
 import { AuthContextProvider } from '@contexts/AuthContext'
 import { Routes } from '@routes/index'
 import { THEME } from './src/theme'
+import OneSignal from 'react-native-onesignal';
+
+OneSignal.setAppId('5e02a7ed-9bd8-407f-9568-3f84725e39c2');
 
 export default function App() {
 	const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold })
